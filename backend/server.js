1f4c685bdead1ed0913195e90f9847cf45fec0f7
yusufbalyemez <yusuf.balyemez93@gmail.com>
2024-02-20 09:11:49 +0000

    Şuanda ip ile çalışıyor

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,40 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require("dotenv");//env için gerekli
+const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const cabBookingRoutes = require('./routes/CabBookingsRoute.js');
 const UserRoutes = require('./routes/UserRoute.js');
-// const deletePastBookings = require('./controllers/cronJobs.js'); // cronJobs.js dosyasını import et
 
-
-
-//env için gerekli
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// Specify your server's IP address or a range of trusted origins
+const allowedOrigins = ['http://138.68.95.100', 'http://localhost', 'https://yuksel-taxi-almanca.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // To allow cookies to be sent and received
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// MongoDB bağlantısı
-const db = mongoose.connect(process.env.DB_URI);
-if(db){
-  console.log("MongoDB connection successfully.")
-}else{
-  console.log("MongoDB connection failed...")
-}
+// MongoDB connection
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log("MongoDB connection successfully."))
+  .catch((err) => console.log("MongoDB connection failed...", err));
 
-// deletePastBookings();
-
-// Route tanımları
-app.use('/bookings', cabBookingRoutes); // Rezervasyonlar için
-app.use('/auth', UserRoutes); // Kullanıcılar için
-
+app.use('/api/bookings', cabBookingRoutes); // For bookings
+app.use('/api/auth', UserRoutes); // For users
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
