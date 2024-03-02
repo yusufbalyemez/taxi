38a0fb8f6ef7f340eaa3fd678f1cd569d23a8b1c
yusufbalyemez <yusuf.balyemez93@gmail.com>
2024-03-03 00:08:06 +0300

    CookieConsent Banner düzeltildi ortaya alındı. Site descriptionları eklendi

--- frontend/src/components/CookieConsent/CookieConsentBanner.jsx
+++ frontend/src/components/CookieConsent/CookieConsentBanner.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useLanguage } from '../Languages/LanguageContext'; // useLanguage hook'unu içe aktarın
 import useCookieConsent from '../../hooks/useCookieConsent';
+import './CookieConsentBanner.css'; 
 
 const CookieConsentBanner = () => {
     const [consent, giveConsent] = useCookieConsent();
@@ -11,9 +12,11 @@ const CookieConsentBanner = () => {
     if (consent === 'true') return null; // Eğer onay verilmişse, bileşeni gösterme
 
     return (
-        <div style={{ position: 'fixed', bottom: '0', left: '0', width: '100%', padding: '20px', backgroundColor: 'black', textAlign: 'center', color: 'white', fontSize: "1.5rem" }}>
-                {text.infoTxt}
-            <button onClick={giveConsent} style={{ marginLeft: '10px', padding: '10px', cursor: 'pointer', borderRadius: '10px' }}>{text.btnTxt}</button>
+        <div className="banner"> {/* className ile CSS sınıfını kullanın */}
+            {language.cookieConsent.infoTxt}
+            <button onClick={giveConsent}>
+                {language.cookieConsent.btnTxt}
+            </button>
         </div>
     );
 };
