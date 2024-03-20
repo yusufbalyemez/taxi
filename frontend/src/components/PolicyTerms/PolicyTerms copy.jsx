08e96db81fa11602157e7d0a1a53531d1f146891
yusufbalyemez <yusuf.balyemez93@gmail.com>
2024-03-21 00:19:16 +0300

    Yeni çerez politikası eklendi

---
+++ frontend/src/components/PolicyTerms/PolicyTerms copy.jsx
@@ -0,0 +1,264 @@
+import React from 'react'
+import "./PolicyTerms.css"
+
+const PolicyTerms = () => {
+  return (
+    <div className='policy-container'>
+            <h1>Datenschutzerklärung</h1>
+
+            <p>Der Schutz und die Sicherheit von persönlichen Daten sind uns ein wichtiges Anliegen. Diese Datenschutzerklärung entspricht den Regeln der EU-Datenschutz-Grundverordnung (DSGVO) und der neuen Fassung des Bundesdatenschutzgesetzes (BDSG 2018). Nachfolgend werden Sie über Art, den Zweck und die Verwendung personenbezogener Daten informiert.</p>
+
+
+            <p>Obwohl unsere Seite mit verschieden Sicherheitsvorkehrungen ausgestattet ist, kann kein vollständiger Schutz Ihrer Daten gewährleistet werden, da Sicherheitslücken im Internet nicht ausgeschlossen werden können. Sollten Sie Anliegen bzgl. Ihrer Daten haben, finden Sie am Ende des Textes die entsprechenden Kontaktdaten unserer Ansprechpartner.</p>
+
+
+
+            <h2>1.Begriffsbestimmungen</h2>
+
+
+            <p>Wir verwenden in unserer Datenschutzerklärung Begriffe, die im Folgenden erläutert werden:</p>
+
+
+
+            <p>„Personenbezogene Daten“ sind alle Informationen, die sich auf eine identifizierte oder identifizierbare natürliche Person (im Folgenden „betroffene Person“) beziehen; als identifizierbar wird eine natürliche Person angesehen, die direkt oder indirekt, insbesondere mittels Zuordnung zu einer Kennung wie einem Namen, zu einer Kennnummer, zu Standortdaten, zu einer Online-Kennung oder zu einem oder mehreren besonderen Merkmalen identifiziert werden kann, die Ausdruck der physischen, physiologischen, genetischen, psychischen, wirtschaftlichen, kulturellen oder sozialen Identität dieser natürlichen Person sind.</p>
+
+
+
+            <p>„Verarbeitung“ ist jeder mit oder ohne Hilfe automatisierter Verfahren ausgeführten Vorgang oder jede solche Vorgangsreihe im Zusammenhang mit personenbezogenen Daten wie das Erheben, das Erfassen, die Organisation, das Ordnen, die Speicherung, die Anpassung oder Veränderung, das Auslesen, das Abfragen, die Verwendung, die Offenlegung durch Übermittlung, Verbreitung oder eine andere Form der Bereitstellung, den Abgleich oder die Verknüpfung, die Einschränkung, das Löschen oder die Vernichtung.</p>
+
+
+
+            <p>„Verantwortlicher“ ist die natürliche oder juristische Person, Behörde, Einrichtung oder andere Stelle, die allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten entscheidet; sind die Zwecke und Mittel dieser Verarbeitung durch das Unionsrecht oder das Recht der Mitgliedstaaten vorgegeben, so können der Verantwortliche beziehungsweise die bestimmten Kriterien seiner Benennung nach dem Unionsrecht oder dem Recht der Mitgliedstaaten vorgesehen werden.</p>
+
+
+
+            <p>„Empfänger“ ist eine natürliche oder juristische Person, Behörde, Einrichtung oder andere Stelle, denen personenbezogene Daten offengelegt werden, unabhängig davon, ob es sich bei ihr um einen Dritten handelt oder nicht. Behörden, die im Rahmen eines bestimmten Untersuchungsauftrags nach dem Unionsrecht oder dem Recht der Mitgliedstaaten möglicherweise personenbezogene Daten erhalten, gelten jedoch nicht als Empfänger; die Verarbeitung dieser Daten durch die genannten Behörden erfolgt im Einklang mit den geltenden Datenschutzvorschriften gemäß den Zwecken der Verarbeitung.</p>
+
+
+
+            <p>„Dritter“ ist eine natürliche oder juristische Person, Behörde, Einrichtung oder andere Stelle, außer der betroffenen Person, dem Verantwortlichen, dem Auftragsverarbeiter und den Personen, die unter der unmittelbaren Verantwortung des Verantwortlichen oder des Auftragsverarbeiters befugt sind, die personenbezogenen Daten zu verarbeiten.</p>
+
+
+
+            <h2>2.Art, Zweck und Verwendung personenbezogener Daten</h2>
+
+
+            <p>Speicherung von Zugriffsdaten</p>
+
+
+
+            <p>Bei jedem Zugriff eines Nutzers auf eine Seite aus dem Angebot unserer Webseite werden Zugriffsdaten in einer Protokolldatei (Log-Datei) auf unserem Server gespeichert. Diese Daten beinhalten z. B.</p>
+            <ul>
+                <li>den Browsertyp und -version,</li>
+                <li>verwendetes Betriebssystem,</li>
+                <li>die Seite, von der aus die Daten angefordert wurde (Referrer-URL),</li>
+                <li>IP-Adresse des anfragenden Rechners,</li>
+                <li>Zugriffsdatum und -uhrzeit der Serveranfrage und</li>
+                <li>Dateinamen und URL.</li>
+            </ul>
+
+
+            <p>Diese Daten werden ausschließlich zu statistischen Zwecken ausgewertet. Eine Weitergabe an Dritte, zu kommerziellen oder nichtkommerziellen Zwecken, findet nicht statt. Der Betreiber darf Server-Logfiles nur dann länger speichern, herausgeben oder nachträglich auf diese zugreifen, wenn dies im rechtlichen Rahmen gestattet ist (z.B. bei Verdacht auf rechtswidrige Aktivitäten)</p>
+
+
+
+            <p>Kontaktmöglichkeit über die Internetseite</p>
+
+
+
+            <p>Besteht innerhalb unseres Internetangebots die Möglichkeit zur Eingabe persönlicher oder geschäftlicher Daten (E-Mail-Adressen, Namen, Anschriften), erfolgt die Angabe freiwillig. Sofern keine erforderlichen Gründe im Zusammenhang mit einer Geschäftsabwicklung bestehen, können Sie jederzeit die zuvor erteilte Genehmigung Ihrer persönlichen Datenspeicherung mit sofortiger Wirkung schriftlich, per E-Mail oder per Fax, widerrufen. Ihre Daten werden nicht an Dritte weitergeben, es sei denn, eine Weitergabe ist aufgrund gesetzlicher Vorschriften erforderlich.</p>
+
+            <p>Soweit wir für Verarbeitungsvorgänge personenbezogener Daten eine Einwilligung der betroffenen Person einholen, dient Art. 6 Abs. 1 lit. a EU-Datenschutzgrundverordnung (DSGVO) als Rechtsgrundlage.</p>
+
+            <p>Bei der Verarbeitung von personenbezogenen Daten, die zur Erfüllung eines Vertrages, dessen Vertragspartei die betroffene Person ist, erforderlich ist, dient Art. 6 Abs. 1 lit. b DSGVO als Rechtsgrundlage. Dies gilt auch für Verarbeitungsvorgänge, die zur Durchführung vorvertraglicher Maßnahmen erforderlich sind.</p>
+
+            <p>Ist die Verarbeitung zur Wahrung eines berechtigten Interesses der MPG oder eines Dritten erforderlich und überwiegen die Interessen, Grundrechte und Grundfreiheiten des Betroffenen das erstgenannte Interesse nicht, so dient Art. 6 Abs. 1 lit. f DSGVO als Rechtsgrundlage für die Verarbeitung.</p>
+
+            <p>Die personenbezogenen Daten der betroffenen Person werden gelöscht oder gesperrt, sobald der Zweck der Speicherung entfällt. Eine Speicherung kann darüber hinaus erfolgen, wenn dies durch den europäischen oder nationalen Gesetzgeber in unionsrechtlichen Verordnungen, Gesetzen oder sonstigen Vorschriften, denen die MPG unterliegt, vorgesehen wurde. Eine Sperrung oder Löschung der Daten erfolgt auch dann, wenn eine durch die genannten Normen vorgeschriebene Speicherfrist abläuft, es sei denn, dass eine Erforderlichkeit zur weiteren Speicherung der Daten für einen Vertragsabschluss oder eine Vertragserfüllung besteht.</p>
+
+            <p>Auf unserer Webseite sind verschiedene Formulare vorhanden, die Sie zur Kontaktaufnahme mit uns nutzen können. Für die Formulare nutzen wir den Service der JotForm Inc., die als Auftragsverarbeiter der Daten, alle personenbezogenen Daten in Übereinstimmung mit Ihren Verpflichtungen gemäß eines Datenverarbeitungszusatzes konform der Datenschutzgrundverordnung verarbeitet.</p>
+
+            <p>Nimmt ein Nutzer die Möglichkeit der Kontaktaufnahme oder Registrierung durch ein solches Formular wahr, so werden die in der Eingabemaske eingegeben Daten an uns übermittelt und auf Servern der EU gespeichert.  Zudem wird auf diese Datenschutzerklärung verwiesen. Die Daten werden ausschließlich für die Verarbeitung des gemeinsamen Anliegens verwendet.</p>
+
+            <p>Rechtsgrundlage für die Verarbeitung der Daten bei der Nutzung der Formulare ist bei Vorliegen einer Einwilligung des Nutzers Art. 6 Abs. 1 lit. a DSGVO. Die Verarbeitung der personenbezogenen Daten aus der Eingabemaske dient uns allein zur Bearbeitung der jeweiligen Anliegen. Der Nutzer hat jederzeit die Möglichkeit, die Einwilligung zur Verarbeitung der personenbezogenen Daten gegenüber den aufgelisteten Ansprechpartnern zu widerrufen.</p>
+
+            <p>Die Verwaltung und Speicherung Ihrer persönlichen Angaben erfolgt auf Systemen der JotForm Inc, USA. Ihre personenbezogenen Daten werden nur in den gesetzlich erforderlichen Fällen bzw. zur Strafverfolgung aufgrund von Angriffen auf unsere Netzinfrastruktur an staatliche Einrichtungen und Behörden übermittelt. Eine Weitergabe zu anderen Zwecken an Dritte findet nicht statt.</p>
+
+
+
+            <p>Cookies</p>
+
+
+
+            <p>Auf einigen unserer Seiten verwenden wir Cookies, um die Nutzung unserer Webseiten zu erleichtern. Dabei handelt es sich um kleine Textdateien, die nur für die Dauer Ihres Besuchs unserer Webseite auf Ihrer Festplatte hinterlegt und abhängig von der Einstellung Ihres Browser-Programms beim Beenden des Browsers wieder gelöscht werden. Viele Cookies enthalten eine sogenannte Cookie-ID. Eine Cookie-ID ist eine eindeutige Kennung des Cookies. Sie besteht aus einer Zeichenfolge, durch welche Internetseiten und Server dem konkreten Internetbrowser zugeordnet werden können, in dem das Cookie gespeichert wurde. Dies ermöglicht es den besuchten Internetseiten und Servern, den individuellen Browser der betroffenen Person von anderen Internetbrowsern, die andere Cookies enthalten, zu unterscheiden. Ein bestimmter Internetbrowser kann über die eindeutige Cookie-ID wiedererkannt und identifiziert werden.</p>
+
+
+
+            <p>Die meisten Browser sind so eingestellt, dass sie Cookies automatisch akzeptieren. Das Speichern von Cookies kann jedoch deaktiviert oder der Browser so eingestellt werden, dass er auf die Sendung von Cookies hinweist. Dann könnte es jedoch passieren, dass bestimmte Features auf der Seite nicht mehr oder nur noch eingeschränkt funktionieren.</p>
+
+
+
+            <p>Datenschutzbestimmungen zu Einsatz und Verwendung von Google Analytics (mit Anonymisierungsfunktion)</p>
+
+
+
+            <p>Diese Website nutzt den Dienst „Google Analytics“, welcher von der Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) angeboten wird, zur Analyse der Websitebenutzung durch Nutzer. Der Dienst verwendet „Cookies“ – Textdateien, welche auf Ihrem Endgerät gespeichert werden. Die durch die Cookies gesammelten Informationen werden im Regelfall an einen Google-Server in den USA gesandt und dort gespeichert.</p>
+
+
+
+            <p>Auf dieser Website greift die IP-Anonymisierung. Die IP-Adresse der Nutzer wird innerhalb der Mitgliedsstaaten der EU und des Europäischen Wirtschaftsraum gekürzt. Durch diese Kürzung entfällt der Personenbezug Ihrer IP-Adresse. Im Rahmen der Vereinbarung zur Auftragsdatenvereinbarung, welche die Websitebetreiber mit der Google Inc. geschlossen haben, erstellt diese mithilfe der gesammelten Informationen eine Auswertung der Websitenutzung und der Websiteaktivität und erbringt mit der Internetnutzung verbundene Dienstleistungen.</p>
+
+
+
+            <p>Sie haben die Möglichkeit, die Speicherung des Cookies auf Ihrem Gerät zu verhindern, indem Sie in Ihrem Browser entsprechende Einstellungen vornehmen. Es ist nicht gewährleistet, dass Sie auf alle Funktionen dieser Website ohne Einschränkungen zugreifen können, wenn Ihr Browser keine Cookies zulässt.</p>
+
+
+
+            <p>Weiterhin können Sie durch ein Browser-Plugin verhindern, dass die durch Cookies gesammelten Informationen (inklusive Ihrer IP-Adresse) an die Google Inc. gesendet und von der Google Inc. genutzt werden. Folgender Link führt Sie zu dem entsprechenden Plugin: https://tools.google.com/dlpage/gaoptout?hl=de</p>
+
+            <p>Hier finden Sie weitere Informationen zur Datennutzung durch die Google Inc.: https://support.google.com/analytics/answer/6004245?hl=de</p>
+
+
+
+            <p>Löschung und Sperrung</p>
+
+
+
+            <p>Wir verpflichten uns, die personenbezogenen Daten, die für eigene Zwecke verarbeitet werden, zu löschen, sobald ihre Kenntnis für die Erfüllung des Zwecks der Speicherung nicht mehr erforderlich ist. An die Stelle einer Löschung tritt eine Sperrung, soweit einer Löschung gesetzliche, satzungsmäßige oder vertragliche Aufbewahrungsfristen entgegenstehen, Grund zu der Annahme besteht, dass durch eine Löschung schutzwürdige Interessen des Betroffenen beeinträchtigt würden oder eine Löschung wegen der besonderen Art der Speicherung nicht oder nur mit unverhältnismäßig hohem Aufwand möglich ist.</p>
+
+
+
+            <p>Darüber hinaus können Sie jederzeit ihre bei uns erhobenen Daten sperren, berichtigen oder löschen lassen. Eine Löschung erfolgt auch, wenn Sie ihre Einwilligung zur Erhebung, Verarbeitung und Nutzung der personenbezogenen Daten widerrufen. Erfolgt der Widerruf während eines laufenden Geschäftsvorgangs, so erfolgt die Löschung unmittelbar nach dessen Abschluss.</p>
+
+            <p>Weiterführende gesetzliche Löschungs- oder Sperrungsverpflichtungen bleiben hiervon unberührt.</p>
+
+
+
+            <p>Datenweitergabe an Dritte</p>
+
+
+
+            <p>Grundsätzlich werden Ihre Daten von uns nicht an Dritte vermittelt. Außerdem stellen wir durch entsprechende Maßnahmen und regelmäßige Kontrollen sicher, dass die von uns erhobenen Daten nicht durch Dritte von außen eingesehen oder abgegriffen werden können.</p>
+
+
+
+            <p>Wir übermitteln Ihre Daten ausschließlich dann an Dritte, wenn Sie Ihre Einwilligung zu der Verarbeitung der sie betreffenden personenbezogenen Daten für einen oder mehrere bestimmte Zwecke gegeben haben und uns hierfür Ihre Einwilligung vorliegt.</p>
+
+            <p>Wir versichern, dass die so weiter gegebenen Daten den gesetzlichen Vorgaben entsprechen.</p>
+
+
+
+            <p>Betroffenenrechte</p>
+
+
+
+            <p>Laut geltendem Gesetz sind wir dazu verpflichtet, Sie über ihre Rechte aufzuklären. Die Inanspruchnahme und Durchführung dieser Rechte ist für Sie kostenlos.</p>
+
+
+
+            <p>Widerspruchsrecht</p>
+
+
+
+            <p>Sie haben das Recht, Ihre Einwilligung bzgl. der Erhebung von Daten jederzeit zu widerrufen. Dieses Recht gilt mit Wirkung für die Zukunft; die bis zur Rechtkraft des Widerrufs erhobenen Daten bleiben hiervon unberührt.</p>
+
+
+
+            <p>Bitte wenden Sie sich an den u.g. Verantwortlichen oder Datenschutzbeauftragten, wenn Sie von Ihrem Widerspruchsrecht Gebrauch machen möchten.</p>
+
+
+
+            <p>Recht auf Datenübertragbarkeit</p>
+
+
+
+            <p>Sie haben das Recht, eine Übertragung Ihrer Daten von uns auf eine andere Stelle zu beantragen.</p>
+
+
+
+            <p>Recht auf Berichtigung, Löschung oder Sperrung</p>
+
+
+
+            <p>Sie haben das Recht, Ihre Daten berichtigen, löschen oder sperren zu lassen. Letzteres kommt zur Anwendung, wenn die gesetzliche Lage eine Löschung nicht zulässt.</p>
+
+
+
+            <p>Beschwerderecht</p>
+
+
+
+            <p>Sie haben das Recht, sich bei einer Aufsichtsbehörde bzw. einer zuständigen Stelle zu beschweren, insofern Sie einen Grund zur Beanstandung haben sollten. Für die Inanspruchnahme dieses Rechts und der zwei vorher genannten wenden Sie sich bitte an die am Ende dieser Datenschutzerklärung aufgeführten Kontaktpersonen.</p>
+
+
+
+            <h2>3.Name und Anschrift des für die Verarbeitung Verantwortlichen</h2>
+
+
+            <p>Verantwortlicher im Sinne der Datenschutz-Grundverordnung, sonstiger in den Mitgliedstaaten der Europäischen Union geltenden Datenschutzgesetze und anderer Bestimmungen mit datenschutzrechtlichem Charakter ist die:</p>
+
+
+
+            <p>Johann Daniel Lawaetz-Stiftung</p>
+
+            <p>Gemeinnützige Stiftung des bürgerlichen Rechts</p>
+
+
+
+            <p>Geschäftsführender Vorstand:</p>
+
+            <p>Jörg Lindner, Peer Gillner</p>
+
+
+
+            <p>Ansprechpartner für den Datenschutz:</p>
+
+            <p>Peer Gillner</p>
+
+            <p>Gillner@lawaetz.de</p>
+
+
+
+            <p>Neumühlen 16- 20; D-22763 Hamburg</p>
+
+            <p>Telefon + 49 40/ 39 99 36- 0</p>
+
+            <p>Telefax + 49 40/ 39 99 36 90</p>
+
+            <p>Start</p>
+
+
+
+
+
+            <h2>4.Name und Anschrift des Datenschutzbeauftragten</h2>
+
+
+            <p>Der Datenschutzbeauftragte des für die Verarbeitung Verantwortlichen ist:</p>
+
+            <p>Uwe Jochens</p>
+
+            <p>Johann Daniel Lawaetz-Stiftung</p>
+
+            <p>Neumühlen 16- 20; D- 22763 Hamburg</p>
+
+            <p>Telefon + 49 40/ 39 99 36- 0</p>
+
+            <p>Telefax + 49 40/ 39 99 36 90</p>
+
+            <p>dsb@lawaetz.de</p>
+
+            <p>Start</p>
+
+
+
+
+
+            <p>Jede betroffene Person kann sich jederzeit bei allen Fragen und Anregungen zum Datenschutz direkt an unseren Datenschutzbeauftragten wenden.</p>
+
+
+        </div>
+  )
+}
+
+export default PolicyTerms
