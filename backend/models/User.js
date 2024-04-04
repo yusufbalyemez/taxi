0f4aa7525383ecbb419630b841b8c9e9fc32a75b
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-04-04 16:19:08 +0300

    Kullanıcı eklemenin varsayılan değerini admin yaptım

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password:{type: String, required: true},
   phone: {type:String, required:true},
-  role: {type: String, default: "user", enum: ["user","admin"]}
+  role: {type: String, default: "admin", enum: ["user","admin"]}
 
 }, {
   timestamps: true // Zaman damgalarını etkinleştir
